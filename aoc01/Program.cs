e4aae56d83ef356083ac3a5636853c90e9b1f828
Jeroen <JeroenJacobs@Outlook.com>
2020-12-01 18:48:08 +0100

    Added Aoc01 part 2.

--- aoc01/Program.cs
+++ aoc01/Program.cs
@@ -1,25 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace aoc01
+void part1(IReadOnlyList<int> numbers)
 {
-  class Program
+  for (int i = 0; i < numbers.Count; i++)
   {
-    static void Main(string[] args)
+    for (int j = i + 1; j < numbers.Count; j++)
     {
-      var numbers = File.ReadLines("input.txt").Select(int.Parse).ToList();
-
-      for (int i = 0; i < numbers.Count; i++)
+      if (numbers[i] + numbers[j] == 2020)
       {
-        for (int j = i+1; j < numbers.Count; j++)
+        Console.WriteLine($"Part 1: the answer is: {numbers[i]} * {numbers[j]} = {numbers[i] * numbers[j]}.");
+      }
+    }
+  }
+}
+
+void part2(IReadOnlyList<int> numbers)
+{
+  for (int i = 0; i < numbers.Count; i++)
+  {
+    for (int j = i + 1; j < numbers.Count; j++)
+    {
+      for (int k = j + 1; k < numbers.Count; k++)
+      {
+        if (numbers[i] + numbers[j] + numbers[k] == 2020)
         {
-          if (numbers[i] + numbers[j] == 2020)
-          {
-            Console.WriteLine($"The answer is: {numbers[i]} * {numbers[j]} = {numbers[i] * numbers[j]}.");
-          }
+          Console.WriteLine($"Part 2: the answer is: {numbers[i]} * {numbers[j]} * {numbers[k]} = {numbers[i] * numbers[j] * numbers[k]}.");
         }
       }
     }
   }
 }
+
+var numbers = File.ReadLines("input.txt").Select(int.Parse).ToList();
+
+part1(numbers);
+part2(numbers);
