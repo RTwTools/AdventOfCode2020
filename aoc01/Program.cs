c6baf8a2c34946f7639c9fef55e9cae5b3804621
Jeroen <JeroenJacobs@Outlook.com>
2020-12-01 18:41:44 +0100

    Added Aoc01 part 1.

---
+++ aoc01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var numbers = File.ReadLines("input.txt").Select(int.Parse).ToList();
+
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        for (int j = i+1; j < numbers.Count; j++)
+        {
+          if (numbers[i] + numbers[j] == 2020)
+          {
+            Console.WriteLine($"The answer is: {numbers[i]} * {numbers[j]} = {numbers[i] * numbers[j]}.");
+          }
+        }
+      }
+    }
+  }
+}
