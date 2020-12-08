998556d9e7680301f6a4bbbc947dee620c0e7cea
Jeroen <JeroenJacobs@Outlook.com>
2020-12-08 08:12:12 +0100

    Added Aoc08 part 2.

--- aoc08/Program.cs
+++ aoc08/Program.cs
@@ -9,48 +9,111 @@ namespace aoc08
   {
     public static void Main()
     {
-      var instructions = File.ReadAllLines("input.txt").Select(Instruction.Parse).ToList();
+      IReadOnlyList<Instruction> instructions = File.ReadAllLines("input.txt").Select(Instruction.Parse).ToList();
 
-      Console.WriteLine($"Part 1: the value of the accumulator is {HandHeld.Execute(instructions)}.");
+      Console.WriteLine($"Part 1: the value of the accumulator is {HandHeld.Execute(instructions).accumulator}.");
+
+      int instruction = 1;
+      while (instruction <= instructions.Count)
+      {
+        var newInstructions = instructions.Select(i => new Instruction(i.Command, i.Value)).ToList();
+        var counter = 0;
+
+        for (int i = 0; i < newInstructions.Count; i++)
+        {
+          if (newInstructions[i].Command == "jmp" || newInstructions[i].Command == "nop")
+          {
+            counter++;
+
+            if (counter >= instruction)
+            {
+              if (newInstructions[i].Command == "nop" && newInstructions[i].Value == 0)
+              {
+                instruction++;
+                continue;
+              }
+
+              newInstructions[i].Command = (newInstructions[i].Command == "jmp")
+                ? "nop"
+                : "jmp";
+
+              break;
+            }
+          }
+        }
+
+        var result = HandHeld.Execute(newInstructions);
+
+        if (result.ok)
+        {
+          Console.WriteLine($"Part 2: the value of the accumulator is {result.accumulator}.");
+
+          break;
+        }
+        else
+        {
+          instruction++;
+        }
+      }
+
     }
   }
 
   public class HandHeld
   {
-    public static int Execute(IList<Instruction> instructions)
+    public static (int accumulator, bool ok) Execute(IReadOnlyList<Instruction> instructions)
     {
-      var Instructions = new List<Instruction>(instructions);
+      var Instructions = instructions.Select(i => new Instruction(i.Command, i.Value)).ToList();
 
       int currentInstruction = 0;
       int accumulator = 0;
 
-      while (!Instructions[currentInstruction].Executed)
+      while (currentInstruction < Instructions.Count && !Instructions[currentInstruction].Executed)
       {
-        accumulator += Instructions[currentInstruction].Value;
         Instructions[currentInstruction].Executed = true;
-        currentInstruction += Instructions[currentInstruction].NextInstruction;
+
+        switch (Instructions[currentInstruction].Command)
+        {
+          case "acc":
+            accumulator += Instructions[currentInstruction].Value;
+            currentInstruction++;
+            break;
+          case "jmp":
+            currentInstruction += Instructions[currentInstruction].Value;
+            break;
+          default:
+            currentInstruction++;
+            break;
+        }
       }
 
-      return accumulator;
+      var ok = currentInstruction == Instructions.Count;
+
+      return (accumulator, ok);
     }
   }
 
-  public record Instruction(int Value, int NextInstruction = 1)
+  public class Instruction
   {
+    public string Command { get; set; }
+    public int Value { get; set; }
     public bool Executed { get; set; } = false;
+
+    public Instruction(string command, int value)
+    {
+      Command = command;
+      Value = value;
+    }
 
     public static Instruction Parse(string data)
     {
       var splitData = data.Split(" ");
-      var value = int.Parse(splitData[1]);
+      return new Instruction(splitData[0], int.Parse(splitData[1]));
+    }
 
-      return (splitData[0]) switch
-      {
-        "acc" => new Instruction(value),
-        "jmp" => new Instruction(0, value),
-        "nop" => new Instruction(0),
-        _ => throw new InvalidDataException(),
-      };
+    public override string ToString()
+    {
+      return $"{Command}:{Value}:{Executed}";
     }
   }
 }
