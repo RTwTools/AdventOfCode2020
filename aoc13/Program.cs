c7b4b63483ff4a3d5b338ba6e8d9b1024b945d76
Jeroen <JeroenJacobs@Outlook.com>
2020-12-13 08:33:07 +0100

    Added Aoc13 part 1.

---
+++ aoc13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc13
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      var data = File.ReadAllLines("input.txt");
+
+      int earliestDepartmentTime = int.Parse(data[0]);
+      var busIds = data[1].Split(new[] { ',', 'x' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+
+      var busDepartData = busIds.Select(busId => (busId, ((((earliestDepartmentTime - 1) / busId) + 1) * busId) - earliestDepartmentTime)).OrderBy(b => b.Item2);
+      
+      Console.WriteLine($"Part 1: the busId multiplied by the waiting time is {busDepartData.First().busId * busDepartData.First().Item2}");
+    }
+  }
+}
